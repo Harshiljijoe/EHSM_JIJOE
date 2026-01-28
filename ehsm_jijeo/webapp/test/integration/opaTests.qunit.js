09d101819503584f35d2a3a674a70ee3f3fa000b
HARSHIL JIJOE <harshiljijoe01@gmail.com>
2026-01-28 05:52:42 +0000

    new

---
+++ ehsm_jijeo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["ehsmjijeo/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
---
+++ ehsm_jijeo/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ehsmjijeo/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
