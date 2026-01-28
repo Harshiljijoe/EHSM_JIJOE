083b91a61346fad7d37e61f933ac83e5aeb9f0fc
HARSHIL JIJOE <harshiljijoe01@gmail.com>
2026-01-28 05:40:24 +0000

    Initial

--- ehsm_pp6/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["ehsmpp6/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- ehsm_pp6/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"ehsmpp6/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
